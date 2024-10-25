93f15452a1c87900a2a29981dadc3c75380eaaf1
sekiyuki-2013 <147236095+sekiyuki-2013@users.noreply.github.com>
2024-10-25 12:42:51 +0900

    Update hinto.js
    ヒントを増やしました

--- src/hinto.js
+++ src/hinto.js
@@ -2,11 +2,11 @@
 
 //ヒントの書き方
 //hinto_kanri.push(new Hinto(1800 - 表示したい秒数*10, "表示したい文字"));
-hinto_kanri.push(new Hinto(1800 - 100, "10秒経過"));
+hinto_kanri.push(new Hinto(1800 - 300, "サイコロは1-6,2-5,3-4が対になっている"));
 
-hinto_kanri.push(new Hinto(1800 - 50, "5秒経過"));
+hinto_kanri.push(new Hinto(1800 - 900, "この空間は赤-緑,黄-青,白-紫が対になっている"));
 
-hinto_kanri.push(new Hinto(1500, "残り2分半"));
+hinto_kanri.push(new Hinto(1800 - 1200, "4は3の対,3が白なので,白の対（白の上）は何色かな？"));
 
 
 
